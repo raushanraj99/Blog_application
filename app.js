328d9f5db791561d12213f94546303c093b6a4f4
raushanraj99 <raushanraj9950@gmail.com>
2023-11-18 14:41:01 +0530

    Initial commit

--- app.js
+++ app.js
@@ -60,15 +60,5 @@ app.get("/",async(req,res)=>{
 
 
 
-// const app = require("./app")
-const DBconnect = require("./src/database/databaseConn")
-DBconnect()
-
-
-
-app.listen(process.env.PORT || 8000,()=>{
-   console.log(`Server is working fine on ${process.env.PORT}`) 
-})
-
 
 
